e1ef0590e48cfcf7cccf7679b31b529c584112bc
hashimor625 <hashimor625@gmail.com>
2018-12-14 00:30:18 +0900

    Cinemachine

---
+++ Assets/Scripts/Others/FrameAdvance.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrameAdvance : MonoBehaviour
+{
+    [SerializeField]
+    private int framePerSecond;
+    [SerializeField]
+    private float secondPerFrame;
+
+    private void OnValidate()
+    {
+        
+    }
+
+    private float time;
+
+    void Update()
+    {
+    }
+
+    private void SetOffset(Vector2 offset)
+    {
+    }
+
+    private void OnEnable()
+    {
+    }
+}
